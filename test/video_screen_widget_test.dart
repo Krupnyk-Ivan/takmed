aa056dc95e3e60888f1534d3e1f643e4ae8f2e08
Krupnyk-Ivan <147716414+Krupnyk-Ivan@users.noreply.github.com>
2025-05-13 14:35:55 +0300

    work

--- test/video_screen_widget_test.dart
+++ test/video_screen_widget_test.dart
@@ -1,65 +1,58 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:takmed/info_pages/video_screen_widget.dart'; // Update this import if necessary
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:video_player/video_player.dart';
+
 void main() {
-  test('Test description for Question Model', () {
-    // Your test logic here
-    expect(true, true);
+  testWidgets('VideoScreenWidget test', (WidgetTester tester) async {
+    // Build the widget tree for VideoScreenWidget
+    await tester.pumpWidget(MaterialApp(home: VideoScreenWidget()));
+    // Ensure the widget tree has loaded the proper video player control (flick manager)
+    final flickManagerFinder = find.byType(FlickManager);
+
+    // Access the FlickManager from the widget tree
+    final flickManager =
+        tester
+            .widget<FlickVideoPlayer>(find.byType(FlickVideoPlayer))
+            .flickManager;
+
+    // Access the VideoPlayerController from the flick manager
+    final videoPlayerController =
+        flickManager.flickVideoManager?.videoPlayerController;
+    flickManager.flickControlManager?.pause();
+
+    await tester.pumpAndSettle();
+
+    // Check if the FlickVideoPlayer widget is present in the widget tree
+    expect(find.byType(FlickVideoPlayer), findsOneWidget);
+
+    // Check if the Text widget with the specific content is present
+    expect(
+      find.text(
+        'Перше, на що слід звертати увагу, це зупинка масивної кровотечі. '
+        'Це може бути зовнішня або внутрішня кровотеча, і вона є однією з основних причин смерті на полі бою. '
+        'Пріоритетним методом є застосування турнікетів (для кінцівок) або прямий тиск на рани для зупинки кровотечі. '
+        'Якщо кровотеча не зупиняється за допомогою простих методів, слід застосовувати більш складні техніки, '
+        'наприклад, гемостазуючі засоби або операційні заходи (якщо це можливо).',
+      ),
+      findsOneWidget,
+    );
+
+    // Check if the videoPlayerController is available and initialized
+    expect(videoPlayerController, isNotNull);
+
+    // Check if the video is paused by default (should be in paused state when first loaded)
+    expect(videoPlayerController?.value.isPlaying, true);
+    flickManager.flickControlManager?.pause();
+    // Test play/pause functionality (if applicable)
+
+    // Wait for a brief moment and check if the video is playing
+    await tester.pump(
+      Duration(seconds: 1),
+    ); // Use pump() to simulate time passage
+    expect(videoPlayerController?.value.isPlaying, false);
   });
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:takmed/info_pages/video_screen_widget.dart'; // Update this import if necessary
-// import 'package:flick_video_player/flick_video_player.dart';
-// import 'package:video_player/video_player.dart';
-
-// void main() {
-//   testWidgets('VideoScreenWidget test', (WidgetTester tester) async {
-//     // Build the widget tree for VideoScreenWidget
-//     await tester.pumpWidget(MaterialApp(home: VideoScreenWidget()));
-//     // Ensure the widget tree has loaded the proper video player control (flick manager)
-//     final flickManagerFinder = find.byType(FlickManager);
-
-//     // Access the FlickManager from the widget tree
-//     final flickManager =
-//         tester
-//             .widget<FlickVideoPlayer>(find.byType(FlickVideoPlayer))
-//             .flickManager;
-
-//     // Access the VideoPlayerController from the flick manager
-//     final videoPlayerController =
-//         flickManager.flickVideoManager?.videoPlayerController;
-//     flickManager.flickControlManager?.pause();
-
-//     await tester.pumpAndSettle();
-
-//     // Check if the FlickVideoPlayer widget is present in the widget tree
-//     expect(find.byType(FlickVideoPlayer), findsOneWidget);
-
-//     // Check if the Text widget with the specific content is present
-//     expect(
-//       find.text(
-//         'Перше, на що слід звертати увагу, це зупинка масивної кровотечі. '
-//         'Це може бути зовнішня або внутрішня кровотеча, і вона є однією з основних причин смерті на полі бою. '
-//         'Пріоритетним методом є застосування турнікетів (для кінцівок) або прямий тиск на рани для зупинки кровотечі. '
-//         'Якщо кровотеча не зупиняється за допомогою простих методів, слід застосовувати більш складні техніки, '
-//         'наприклад, гемостазуючі засоби або операційні заходи (якщо це можливо).',
-//       ),
-//       findsOneWidget,
-//     );
-
-//     // Check if the videoPlayerController is available and initialized
-//     expect(videoPlayerController, isNotNull);
-
-//     // Check if the video is paused by default (should be in paused state when first loaded)
-//     expect(videoPlayerController?.value.isPlaying, true);
-//     flickManager.flickControlManager?.pause();
-//     // Test play/pause functionality (if applicable)
-
-//     // Wait for a brief moment and check if the video is playing
-//     await tester.pump(
-//       Duration(seconds: 1),
-//     ); // Use pump() to simulate time passage
-//     expect(videoPlayerController?.value.isPlaying, false);
-//   });
-// }
